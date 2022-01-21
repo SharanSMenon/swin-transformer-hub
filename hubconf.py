77448a5ac2899442ed20aa7db7037b11774ee382
sharansajivmenon <sharansajivmenon@gmail.com>
2022-01-21 15:16:58 -0500

    bugfix

--- hubconf.py
+++ hubconf.py
@@ -10,5 +10,5 @@ MODEL_URLS = {
 def swin_tiny_patch4_window7_224(pretrained=False):
     model = swin.SwinTransformer()
     if pretrained:
-        model.load_state_dict(torch.hub.load_state_dict_from_url(MODEL_URLS["swin_tiny_patch4_window7_224"], map_location="cpu"))
+        model.load_state_dict(torch.hub.load_state_dict_from_url(MODEL_URLS["swin_tiny_patch4_window7_224"], map_location="cpu")['model'])
     return model
