e57bd7c29e5189c7b5538c91c015c45ba9c0c4de
sharansajivmenon <sharansajivmenon@gmail.com>
2022-01-21 15:11:38 -0500

    hubconf added

---
+++ README.md
@@ -0,0 +1,8 @@
+## Swin Transformer PyTorch Hub
+
+This is just a quick way to load Swin Transformers from image classification from PyTorch Hub. This repository makes it possible to load Swin Transformers in 1 line of code. 
+
+```python
+import torch
+model = torch.hub.load() # not yet ready
+```
---
+++ hubconf.py
@@ -0,0 +1,12 @@
+import torch
+import swin
+
+MODEL_URLS = {
+    "swin_tiny_patch4_window7_224": "https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth"
+}
+
+def swin_tiny_patch4_window7_224(pretrained=False):
+    model = swin.SwinTransformer()
+    if pretrained:
+        model.load_state_dict(torch.hub.load_state_dict_from_url(MODEL_URLS["swin_tiny_patch4_window7_224"], map_location="cpu"))
+    return model
